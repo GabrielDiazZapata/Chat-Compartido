9d1e7e349386449960eddd87d8fb85728987f262
GabrielDiazZapata <usr3566@salesianos-lacuesta.net>
2024-01-10 13:03:51 +0000

    feat: (ClientHandler.Java) set time format and improved screen output

--- src/server/thread/ClientHandler.java
+++ src/server/thread/ClientHandler.java
@@ -3,7 +3,9 @@ package server.thread;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.Socket;
-
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 public class ClientHandler extends Thread{
     private Socket clientSocket;
 
@@ -11,17 +13,20 @@ public class ClientHandler extends Thread{
         this.clientSocket = socket;
     }
 
+     DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss" );
+     Date date = new Date();
+
     @Override
     public void run() {
         try {
             DataInputStream dataInputStream = new DataInputStream(this.clientSocket.getInputStream());
             final String username = dataInputStream.readUTF();
-            System.out.println("Etoy aquiii "+ username);
+            System.out.println("<"+username+">");
             String mensajeRecibido ="";
             while (!mensajeRecibido.equals("bye")){
                 mensajeRecibido  =dataInputStream.readUTF();
-
-                System.out.println(username + " envia " + mensajeRecibido);
+                
+                System.out.println( dateFormat.format(date ) +" "+   username+":" + '<' + mensajeRecibido + '>');
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
